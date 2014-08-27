37b126ccdfb9a572326adc1324760fc7f33486fe
Jim Counts <jamesrcounts@outlook.com>
2014-08-26 21:42:35 -0600

    file size check

--- spec/fileapprover_spec.lua
+++ spec/fileapprover_spec.lua
@@ -1,14 +1,23 @@
 local  fileapprover  =  require 'approvals.fileapprover'
+local utilities = require 'approvals.utilities'
 
 describe( 'A file approver',  function(  )
 	it( 'fails on missing expected file',  function (    )
 		local  actual  =  fileapprover.verify_files( nil,  'foo.txt' )
-		assert.equal( 'Approved file does not exist: foo.txt',  actual )
+		assert.equal( 'Approved expectation does not exist: foo.txt',  actual )
 
 	end ) 
 
 	it( 'fails when file sizes differ',  function (    )
-		local  actual  =  fileapprover.verify_files( 'r.txt',  'a.txt' )
-		assert.equal( 'File sizes do not match: \r\na.txt(5)\r\\nb.txt(3)',  actual )
+		local actual = utilities.adjacent_path('a.txt')
+		local a = assert(io.open(actual, 'w'))
+		a:write('hello')
+		a:close()
+		local expected = 	utilities.adjacent_path('e.txt')
+		local e = assert(io.open(expected, 'w'))
+		e:write('hello world')
+		e:close()
+		local  actual  =  fileapprover.verify_files( actual,  expected )
+		assert.equal( 'File sizes do not match:\r\n.\\spec\\e.txt(11)\r\n.\\spec\\a.txt(5)',  actual )
 	end )
 end )
--- src/approvals/fileapprover.lua
+++ src/approvals/fileapprover.lua
@@ -1,7 +1,19 @@
-local function verify_files( received_file, approved_file )
-	return 'Approved file does not exist: ' .. approved_file;
+local   path   =   require(  'pl.path'  )
+
+local   function verify_files(   actual_file,   expected_file   )
+	local   message   =   nil
+
+	if not path.exists(  expected_file  ) then
+		message   =   	 'Approved expectation does not exist: ' .. expected_file;  
+		return message
+	end
+
+	local   e_size   =   path.getsize(  expected_file  )
+	local  a_size  =  path.getsize( actual_file )
+
+	return string.format(  'File sizes do not match:\r\n%s(%d)\r\n%s(%d)',   expected_file,  e_size,  actual_file,  a_size  );  
 end
 
-return {
-	verify_files = verify_files
-}
+return {  
+	verify_files   =   verify_files
+  }
