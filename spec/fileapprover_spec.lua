4e68a9d37c403a8fee4ab71c9b32930668a3b29e
Jim Counts <jamesrcounts@outlook.com>
2014-08-27 06:58:00 -0600

    size mismatch

--- spec/fileapprover_spec.lua
+++ spec/fileapprover_spec.lua
@@ -1,6 +1,14 @@
 local  fileapprover  =  require 'approvals.fileapprover'
 local utilities = require 'approvals.utilities'
 
+local function create_text_file( name, contents )
+		local actual = utilities.adjacent_path(name)
+		local a = assert(io.open(actual, 'w'))
+		a:write(contents)
+		a:close()
+		return actual
+end
+
 describe( 'A file approver',  function(  )
 	it( 'fails on missing expected file',  function (    )
 		local  actual  =  fileapprover.verify_files( nil,  'foo.txt' )
@@ -9,15 +17,29 @@ describe( 'A file approver',  function(  )
 	end ) 
 
 	it( 'fails when file sizes differ',  function (    )
-		local actual = utilities.adjacent_path('a.txt')
-		local a = assert(io.open(actual, 'w'))
-		a:write('hello')
-		a:close()
-		local expected = 	utilities.adjacent_path('e.txt')
-		local e = assert(io.open(expected, 'w'))
-		e:write('hello world')
-		e:close()
-		local  actual  =  fileapprover.verify_files( actual,  expected )
-		assert.equal( 'File sizes do not match:\r\n.\\spec\\e.txt(11)\r\n.\\spec\\a.txt(5)',  actual )
+		
+		local actual = create_text_file('a.txt', 'hello')
+		local expected = create_text_file('e.txt', 'hello world')
+
+		local  message  =  fileapprover.verify_files( actual,  expected )
+		assert.equal( 'File sizes do not match:\r\n.\\spec\\e.txt(11)\r\n.\\spec\\a.txt(5)',  message )
 	end )
+
+	it('fails when file contents differ', function (  )
+		local actual = create_text_file('ac.txt','12345')
+		local expected = create_text_file('ec.txt','54321')
+
+		local message = fileapprover.verify_files( actual, expected )
+		assert.equal( 'File contents do not match:\r\n.\\spec\\ec.txt(5)\r\n.\\spec\\ac.txt(5)', message);
+	end)
+
+
+	it('passes when files are same', function (  )
+		local actual = create_text_file('as.txt','12345')
+		local expected = create_text_file('es.txt','12345')
+
+		local message = fileapprover.verify_files( actual, expected )
+		assert.falsy(message);
+	end)
+
 end )
--- src/approvals/fileapprover.lua
+++ src/approvals/fileapprover.lua
@@ -1,17 +1,23 @@
 local   path   =   require(  'pl.path'  )
 
+local approval_missing = 'Approved expectation does not exist: %s'
+local size_mismatch = 'File sizes do not match:\r\n%s(%d)\r\n%s(%d)'
+local content_mismatch = 'File contents do not match:\r\n%s(%d)\r\n%s(%d)'
+
 local   function verify_files(   actual_file,   expected_file   )
 	local   message   =   nil
 
 	if not path.exists(  expected_file  ) then
-		message   =   	 'Approved expectation does not exist: ' .. expected_file;  
-		return message
+		return string.format(approval_missing, expected_file)
 	end
 
-	local   e_size   =   path.getsize(  expected_file  )
-	local  a_size  =  path.getsize( actual_file )
+	local e_size = path.getsize( expected_file )
+	local a_size = path.getsize( actual_file )
+	if  e_size ~= a_size then
+		return string.format(size_mismatch, expected_file, e_size, actual_file, a_size);  
+	end
 
-	return string.format(  'File sizes do not match:\r\n%s(%d)\r\n%s(%d)',   expected_file,  e_size,  actual_file,  a_size  );  
+	return string.format(content_mismatch, expected_file, e_size, actual_file, a_size);  
 end
 
 return {  
