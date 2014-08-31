dcb098e2282c3973485f4dfdfdcc1852f319c8fe
Jim Counts <jamesrcounts@outlook.com>
2014-08-31 08:49:43 -0600

    Checked for missing expectation

--- spec/fileapprover_spec.lua
+++ spec/fileapprover_spec.lua
@@ -1,13 +1,24 @@
--- local  fileapprover  =  require 'approvals.fileapprover'
--- local utilities = require 'approvals.utilities'
--- local path = require 'pl.path'
+local fileapprover  = require 'approvals.fileapprover'
+local utilities     = require 'approvals.utilities'
+local path          = require 'pl.path'
 
--- local function create_text_file_at( path, contents )
---   local a = assert(io.open(path, 'w'))
---   a:write(contents)
---   a:close()
---   return path
--- end
+local function clean_up_file ( file_path )
+  if path.exists(file_path) then
+    assert(os.remove(file_path))
+  end
+end
+
+local function clean_up ( n, extension )
+  clean_up_file(n.expected_file(extension))
+  clean_up_file(n.actual_file(extension))
+end
+
+local function create_text_file_at( path, contents )
+  local a = assert(io.open(path, 'w'))
+  a:write(contents)
+  a:close()
+  return path
+end
 
 
 -- local function create_text_file( name, contents )
@@ -17,11 +28,6 @@
 
 -- describe( 'A file approver',  function()
 --   describe('file verification', function()
---     it( 'fails on missing expected file',  function ()
---       local  actual  =  fileapprover.verify_files( nil,  'foo.txt' )
---       assert.equal( 'Approved expectation does not exist: foo.txt',  actual )
-
---     end )
 
 --     it( 'fails when file sizes differ',  function ()
 
@@ -43,76 +49,83 @@
 
 --   end)
 
---   describe('verification', function()
---     local namer = {
---       actual_file = function ( extension )
---         return 'verification.actual' .. extension
---       end,
---       expected_file = function ( extension )
---         return 'verification.expected' .. extension
---       end
---     }
+describe('verification', function()
+  local namer = {
+    actual_file = function ( extension )
+      return utilities.adjacent_path('verification.actual' .. extension)
+    end,
+    expected_file = function ( extension )
+      return utilities.adjacent_path('verification.expected' .. extension)
+    end
+  }
 
---     local reporter = {
---       report = function(self, ...)
---         self.called = true
---       end,
---       was_called = function(self)
---         return self.called
---       end
---     }
+  local reporter = {
+    report = function(self, ...)
+      self.called = true
+    end,
+    was_called = function(self)
+      return self.called
+    end
+  }
 
---     local writer = {
---       extension = '.txt',
---       write = function( path )
---         create_text_file_at(path, '12345')
---       end
---     }
+  local writer = {
+    extension = '.txt',
+    write = function( path )
+      create_text_file_at(path, '12345')
+    end
+  }
 
 
---     it('fails when file contents differ', function ()
---       local actual = create_text_file('ac.txt','12345')
---       local expected = create_text_file('ec.txt','54321')
+  it( 'fails on missing expected file',  function ()
+    clean_up(namer, '.txt')
+    assert.has_error(function() fileapprover.verify( writer, namer, reporter ) end,
+      'Approved expectation does not exist: verification.expected.txt')
 
---       assert.falsy(fileapprover.verify( writer, namer, reporter ));
---     end)
+  end )
 
---     it('passes when files are same', function ()
---       local actual = create_text_file(namer.actual_file('.txt'),'12345')
---       local expected = create_text_file(namer.expected_file('.txt'),'12345')
+  -- it('fails when file contents differ', function ()
+  --   local actual = create_text_file('ac.txt','12345')
+  --   local expected = create_text_file('ec.txt','54321')
 
---       print('Verify same files...')
---       assert.truthy( fileapprover.verify( writer, namer, reporter ), 'verified ok');
---       print('they were the same')
---       assert.falsy(reporter:was_called(), 'reporter not called')
---       os.remove(actual)
---       os.remove(expected)
---     end)
+  -- assert.falsy(fileapprover.verify( writer, namer, reporter ));
+  -- end)
 
---     it('launches reporter on failure', function()
---       local expected = namer.expected_file('.txt');
---       create_text_file_at(expected, 'Helol')
+  it('passes when files are same', function ()
+    --       local actual = create_text_file(namer.actual_file('.txt'),'12345')
+          local expected = create_text_file_at(namer.expected_file('.txt'),'12345')
 
---       fileapprover.verify( writer, namer, reporter );
---       assert.truthy(reporter:was_called())
---     end)
+    --       print('Verify same files...')
+    assert.truthy( fileapprover.verify( writer, namer, reporter ), 'verified ok');
+    --       print('they were the same')
+    assert.falsy(reporter:was_called(), 'reporter not called')
+    --       os.remove(actual)
+    --       os.remove(expected)
+  end)
 
---     it( 'Removes actual file on match', function ( )
---       local expected = namer.expected_file('.txt')
---       create_text_file_at( expected, 'Hello' )
+  --     it('launches reporter on failure', function()
+  --       local expected = namer.expected_file('.txt');
+  --       create_text_file_at(expected, 'Helol')
 
---       fileapprover.verify( writer, namer, reporter )
+  --       fileapprover.verify( writer, namer, reporter );
+  --       assert.truthy(reporter:was_called())
+  --     end)
 
---       assert.falsy(path.exists(namer.actual_file('.txt')))
---     end)
+  --     it( 'Removes actual file on match', function ( )
+  --       local expected = namer.expected_file('.txt')
+  --       create_text_file_at( expected, 'Hello' )
 
---     it( 'Preserves approved file on match', function()
---       local expected_file = namer.expected_file('.txt');
---       create_text_file_at( expected_file, 'Hello' );
+  --       fileapprover.verify( writer, namer, reporter )
 
---       fileapprover.verify( writer, namer, reporter );
+  --       assert.falsy(path.exists(namer.actual_file('.txt')))
+  --     end)
 
---       assert.truthy(path.exists(namer.expected_file('.txt')))
---     end)
---   end)
--- end)
+  --     it( 'Preserves approved file on match', function()
+  --       local expected_file = namer.expected_file('.txt');
+  --       create_text_file_at( expected_file, 'Hello' );
+
+  --       fileapprover.verify( writer, namer, reporter );
+
+  --       assert.truthy(path.exists(namer.expected_file('.txt')))
+  --     end)
+  --   end)
+end)
--- src/approvals/fileapprover.lua
+++ src/approvals/fileapprover.lua
@@ -1,77 +1,78 @@
 local path = require( 'pl.path' )
 
 local approval_missing = 'Approved expectation does not exist: %s'
-local size_mismatch = 'File sizes do not match:\r\n%s(%d)\r\n%s(%d)'
-local content_mismatch = 'File contents do not match:\r\n%s(%d)\r\n%s(%d)'
+-- local size_mismatch = 'File sizes do not match:\r\n%s(%d)\r\n%s(%d)'
+-- local content_mismatch = 'File contents do not match:\r\n%s(%d)\r\n%s(%d)'
 
-local function read_all_binary( path )
-  local a = assert(io.open(path,  "rb"))
-  local actual = a:read("*all")
-  return actual
-end
+-- local function read_all_binary( path )
+--   local a = assert(io.open(path,  "rb"))
+--   local actual = a:read("*all")
+--   return actual
+-- end
 
-local function contents_same( actual_file, expected_file )
+-- local function contents_same( actual_file, expected_file )
 
-  local actual = read_all_binary(actual_file)
-  local expect = read_all_binary(expected_file)
+--   local actual = read_all_binary(actual_file)
+--   local expect = read_all_binary(expected_file)
 
-  local an = #actual
-  if an ~= #expect then
-    return false
-  end
+--   local an = #actual
+--   if an ~= #expect then
+--     return false
+--   end
 
-  for i=1,an do
-    if actual:sub(i,i) ~= expect:sub(i,i) then
-      return false
-    end
-  end
+--   for i=1,an do
+--     if actual:sub(i,i) ~= expect:sub(i,i) then
+--       return false
+--     end
+--   end
 
-  return true
-end
+--   return true
+-- end
 
 local function verify_files(actual_file, expected_file)
-  local message = nil
+--   local message = nil
 
   if not path.exists(expected_file) then
     return string.format(approval_missing, expected_file)
   end
 
-  local e_size = path.getsize( expected_file )
-  local a_size = path.getsize( actual_file )
-  if  e_size ~= a_size then
-    e_size = e_size or 0
-    a_size = a_size or 0
-    return string.format(size_mismatch, expected_file, e_size, actual_file, a_size);
-  end
+--   local e_size = path.getsize( expected_file )
+--   local a_size = path.getsize( actual_file )
+--   if  e_size ~= a_size then
+--     e_size = e_size or 0
+--     a_size = a_size or 0
+--     return string.format(size_mismatch, expected_file, e_size, actual_file, a_size);
+--   end
 
-  if not contents_same(actual_file, expected_file) then
-    return string.format(content_mismatch, expected_file, e_size, actual_file, a_size);
-  end
+--   if not contents_same(actual_file, expected_file) then
+--     return string.format(content_mismatch, expected_file, e_size, actual_file, a_size);
+--   end
 
-  return nil
+--   return nil
 end
 
 local function verify( writer, namer, reporter )
-  local actual_file = namer.actual_file(writer.extension)
-  writer.write(actual_file)
+--   local actual_file = namer.actual_file(writer.extension)
+--   writer.write(actual_file)
   local expected_file = namer.expected_file(writer.extension)
 
   local message = verify_files(actual_file, expected_file)
 
-  local ok = not message
-  if not ok then
-    reporter:report( actual_file, expected_file)
-    print (message)
-  else
-    os.remove(actual_file)
+--   local ok = not message
+  if message ~= nil then
+--     reporter:report( actual_file, expected_file)
+    error (message)
+--   else
+--     os.remove(actual_file)
   end
 
-  return ok
+--   return ok
+  return true
 end
 
 
 
 return {
   verify = verify,
-  verify_files = verify_files
+--   verify_files = verify_files
 }
