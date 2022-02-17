43c51b5775c2c6a60bb8091fa4cc45a69584784a
Scott Wierschem <scottw@potentialpower.com>
2022-02-17 16:04:42 -0600

    Update approval_utilities_spec.lua

--- spec/approval_utilities_spec.lua
+++ spec/approval_utilities_spec.lua
@@ -3,12 +3,12 @@ local ut = require 'approvals.utilities'
 describe('Path utilities', function (  )
   it('gets the script directory', function (  )
     local actual = ut.caller_directory()
-    local expected = [[.\spec]]
+    local expected = [[spec]]
     assert.equal(expected,actual)
   end)
   it('gets the adjacent file', function (  )
     local actual = ut.adjacent_path('foo.txt')
-    local expected = [[.\spec\foo.txt]]
+    local expected = [[spec/foo.txt]]
     assert.equal(expected,actual)
   end)
 end)
